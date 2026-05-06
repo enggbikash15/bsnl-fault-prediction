de32635926783f61e19bd4d9aa203657aaf1e81c
enggbikash15 <bikash.satyam6@gmail.com>
2026-05-06 23:23:48 +0530

    Implement fault prediction endpoint
    Added a new endpoint for fault prediction based on signal strength and fiber loss.

--- app.py
+++ app.py
@@ -1,10 +1,28 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+import random
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
     return "BSNL AI Fault Prediction Running Successfully 🚀"
+
+@app.route('/predict', methods=['POST'])
+def predict():
+
+    data = request.get_json()
+
+    signal = int(data['signal_strength'])
+    fiber = float(data['fiber_loss'])
+
+    if signal < 40 or fiber > 3:
+        result = "Fault Detected"
+    else:
+        result = "No Fault"
+
+    return jsonify({
+        "prediction": result
+    })
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=10000)
